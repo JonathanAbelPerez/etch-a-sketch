1276d9c4ac3ee934940b3c1fd5bb1dc79937f02d
Jonathan Perez <jonathanabelerez@gmail.com>
2022-10-02 00:09:07 -0300

    added black and color options

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const container = document.querySelector('#container');
-
+let selectedColor = "black"
 let gridSize = 16;
+const btnColor = document.querySelector('#color')
+const btnBlack = document.querySelector('#black')
+
+btnColor.addEventListener('click', () => {
+    selectedColor = "random"
+})
+btnBlack.addEventListener('click', () => {
+    selectedColor = "black"
+})
+
 
 function createGrid() {
     for (i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement('div')
         div.classList.add("grid-div")
-        div.addEventListener('mouseover', randomColor)
-        div.addEventListener('mousedown', randomColor)
+        //div.addEventListener('mouseover', randomColor)
+        //div.addEventListener('mousedown', randomColor)
         container.appendChild(div)
         container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     }
@@ -19,14 +29,14 @@ function removeGrid() {
     }
 }
 
-function randomColor(e) {
+function randomColor() {
     const randomR = Math.floor(Math.random() * 256)
     const randomG = Math.floor(Math.random() * 256)
     const randomB = Math.floor(Math.random() * 256)
-    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    return `rgb(${randomR}, ${randomG}, ${randomB})`
 }
 
-function eraseColor(){
+function eraseColor() {
     removeGrid()
     createGrid()
 }
@@ -37,14 +47,33 @@ function promptQtt() {
         num = prompt("insert number of columns/rows, max 100", "0");
         gridSize = parseInt(num)
         if (num < 101 && num > 0) {
-            alert(num); 
+            alert(num);
             removeGrid()
             createGrid()
-            x = 1 
-        }else{
+            x = 1
+        } else {
             alert("the number is not supported, insert a new one")
         }
     }
 }
 
 createGrid()
+
+function paintGrid(e, color) {
+    if (e.target.classList == 'grid-div') {
+        let square = e.target;
+        square.style.backgroundColor = color;
+    }
+}
+
+container.addEventListener('mousedown', event => {
+    paintGridEvent = paintGrid(event, selectedColor);
+    window.addEventListener('mouseover', (e) => {
+        if (selectedColor == 'random') {
+            paintGrid(e, randomColor());
+        } else {
+            paintGrid(e, selectedColor);
+        }
+    });
+});
+
